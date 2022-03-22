467ac21d7909ad70ce20f440156f808f613af573
Igor Oliveira <m.igoroliveira@gmail.com>
2022-03-22 17:10:11 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,52 +5,60 @@
 			<p>It's good to see you again!</p>
 		</div>
 
-		<div class="calendar-widget centered large-text">
-			Calendar
-		</div>
+		<div class="calendar-widget centered large-text">Calendar</div>
 		<div class="events-widget large-text">
 			Events
-			<Events/>
-
+			<Events />
 		</div>
-		<div class="booking-widget centered large-text">
-			Create Booking
-		</div>
+		<div class="booking-widget centered large-text">Create Booking</div>
 	</main>
 </template>
 
 <script>
-import axios from '@nextcloud/axios';
-import Events from './components/Events.vue';
+import axios from "@nextcloud/axios";
+import Events from "./components/Events.vue";
 
 const calendarEvents = [];
 let description = "";
 const currentDate = Date.now().toString().slice(0, -5);
-axios.get('/remote.php/dav/calendars/testsson/personal?'
-+'&export&accept=jcal&componentType=VEVENT&start='+currentDate+'&end='+currentDate+600000)
-  .then(function (response) {
-    // handle success
-	response.data[2].slice(1).forEach(element => {
-		if(Object.keys(element[1]).length > 9){
-			description = element[1][9][3];
-		}
-		else
-			description = "";
-		const startDate = new Date(element[1][5][3]);
-		const endDate = new Date(element[1][6][3]);
-		calendarEvents.unshift({ id: element[1][4][3],
-								title: element[1][8][3],
-								content: description,
-								tstart: startDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}),
-								tend: endDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})});
+axios
+	.get(
+		"/remote.php/dav/calendars/testsson/personal?" +
+			"&export&accept=jcal&componentType=VEVENT&start=" +
+			currentDate +
+			"&end=" +
+			currentDate +
+			600000
+	)
+	.then(function (response) {
+		// handle success
+		response.data[2].slice(1).forEach((element) => {
+			if (Object.keys(element[1]).length > 9) {
+				description = element[1][9][3];
+			} else description = "";
+			const startDate = new Date(element[1][5][3]);
+			const endDate = new Date(element[1][6][3]);
+			calendarEvents.unshift({
+				id: element[1][4][3],
+				title: element[1][8][3],
+				content: description,
+				tstart: startDate.toLocaleTimeString([], {
+					hour: "2-digit",
+					minute: "2-digit",
+				}),
+				tend: endDate.toLocaleTimeString([], {
+					hour: "2-digit",
+					minute: "2-digit",
+				}),
+			});
+		});
 	});
-  });
 
 export default {
-	name: 'App',
-  components: {
-    Events
-  },
+	name: "App",
+	components: {
+		Events,
+	},
 	data() {
 		return {
 			message:
@@ -58,7 +66,7 @@ export default {
 				document
 					.querySelector("head")
 					.getAttribute("data-user-displayname") +
-				"!"
+				"!",
 		};
 	},
 };
@@ -119,7 +127,7 @@ main {
 	grid-row-end: 4;
 }
 
-.event{
+.event {
 	background-color: lightgreen;
 }
 </style>
