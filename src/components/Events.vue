f2f29ea5277ffb776540e1b3758909ce182b8928
Igor Oliveira <m.igoroliveira@gmail.com>
2022-03-23 18:22:48 +0000

    Updated functions

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
-		<ul v-for="item in items" :key="item.id" class="event">
-			<li>{{ item.title }} {{ item.tstart }}</li>
-			<h3>{{ item.content }}</h3>
-			<h4>{{ item.tstart }} - {{ item.tend }}</h4>
+		<ul v-for="event in events" :key="event.id" class="event">
+			<li>{{ event.title }} {{ event.tstart }}</li>
+			<h3>{{ event.description }}</h3>
+			<h4>{{ event.tstart }} - {{ event.tend }}</h4>
 		</ul>
 	</div>
 </template>
@@ -11,54 +11,58 @@
 <script>
 import axios from "@nextcloud/axios";
 
-const calendarEvents = [];
-let description = "";
 const userName = document
 	.querySelector("head")
 	.getAttribute("data-user-displayname");
-const currentDate = Date.now().toString().slice(0, -5);
+const currentDate = Date.now().toString().slice(0, -3);
+const getEventsUrl = (userName, calendarName) =>
+	`/remote.php/dav/calendars/${userName}/${calendarName}?export&accept=jcal&componentType=VEVENT&start=${currentDate}`;
+const calName = "personal";
+const getId = (element) => element[1][4][3];
+const getTitle = (element) => element[1][8][3];
+const getStartDate = (element) =>
+	new Date(element[1][5][3]).toLocaleTimeString([], {
+		hour: "2-digit",
+		minute: "2-digit",
+	});
+const getEndDate = (element) =>
+	new Date(element[1][6][3]).toLocaleTimeString([], {
+		hour: "2-digit",
+		minute: "2-digit",
+	});
+const getEventsObjects = (data) => data[2].slice(1);
+const getDescription = (element) => {
+	if (Object.keys(element[1]).length > 9) {
+		return element[1][9][3];
+	} else return "";
+};
 
 export default {
 	name: "Events",
 	data() {
 		return {
-			items: calendarEvents,
+			events: null,
 		};
 	},
 	mounted() {
-		axios
-			.get(
-				"/remote.php/dav/calendars/" +
-					userName +
-					"/personal?export&accept=jcal&componentType=VEVENT&start=" +
-					currentDate +
-					"&end=" +
-					currentDate +
-					600000
-			)
-			.then(function (response) {
+		this.loadCalendarEvents();
+	},
+
+	methods: {
+		loadCalendarEvents() {
+			axios.get(getEventsUrl(userName, calName)).then((response) => {
 				// handle success
-				response.data[2].slice(1).forEach((element) => {
-					if (Object.keys(element[1]).length > 9) {
-						description = element[1][9][3];
-					} else description = "";
-					const startDate = new Date(element[1][5][3]);
-					const endDate = new Date(element[1][6][3]);
-					calendarEvents.unshift({
-						id: element[1][4][3],
-						title: element[1][8][3],
-						content: description,
-						tstart: startDate.toLocaleTimeString([], {
-							hour: "2-digit",
-							minute: "2-digit",
-						}),
-						tend: endDate.toLocaleTimeString([], {
-							hour: "2-digit",
-							minute: "2-digit",
-						}),
-					});
-				});
+				this.events = getEventsObjects(response.data).map(
+					(element) => ({
+						id: getId(element),
+						title: getTitle(element),
+						description: getDescription(element),
+						tstart: getStartDate(element),
+						tend: getEndDate(element),
+					})
+				);
 			});
+		},
 	},
 };
 </script>
